b6ea76d1a63b385ef164a85f75d12dbdde641875
ZhanzhouFeng <53817829+ZhanzhouFeng@users.noreply.github.com>
2020-01-22 11:24:21 +0800

    Add files via upload

---
+++ golden_number-master/get_numbers.py
@@ -0,0 +1,58 @@
+import numpy
+import os
+import sys
+
+WEIGHT_LENGTH = 50
+PULL_RATIO = 0.001
+PULL_VALUE = 99
+
+
+def predict(value, weight):
+    if len(value) != len(weight):
+        weight = weight[:len(value)]
+    return value @ weight
+
+
+def score(weight):
+    return -sum(abs(history[i] - predict(history[i+1:i+1+WEIGHT_LENGTH], weight)) for i in range(TEST_TIMES))
+
+
+def main(stdin=sys.stdin, test_times=8):
+    global GUESS_COUNT, TEST_TIMES
+    global weights, data, history
+    from random import random
+
+    TEST_TIMES = test_times
+
+    files = os.listdir('weights')
+    weights = numpy.concatenate([numpy.loadtxt(f'weights/{file}') for file in files])
+
+    data = numpy.loadtxt(stdin, skiprows=1, ndmin=2)
+    if len(data) < TEST_TIMES:
+        if not len(data):
+            print('10\t20\n')
+            return
+        TEST_TIMES = 1
+    GUESS_COUNT = data.shape[1] - 1
+    history = data[::-1, 0]
+
+    scores = numpy.fromiter(map(score, weights), float)
+    # print(scores.argmax(), scores.argsort()[-5:], file=sys.stderr)
+    pred = predict(history[:WEIGHT_LENGTH], weights[scores.argmax()])
+    if not 0 < pred < 100:
+        pred = history[0]
+    pred_pulled = (pred / .618 * GUESS_COUNT + (PULL_VALUE - pred)) / GUESS_COUNT * .618
+    pred_pulled = predict(history[:WEIGHT_LENGTH], weights[scores.argsort()[-2]])
+    if not 0 < pred_pulled < 100:
+        pred_pulled = history[0]
+    if random() < PULL_RATIO:
+        print(pred_pulled, PULL_VALUE, sep='\t')
+    else:
+        print(pred, pred_pulled, sep='\t')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(test_times=int(sys.argv[1]))
+    else:
+        main()
---
+++ golden_number-master/test.py
@@ -0,0 +1,41 @@
+import numpy
+import os
+
+
+def test_round(round):
+    numpy.savetxt(f'_test_input_{ID}', data[:round], delimiter='\t', header=f'{round}\t{column_count}')
+    os.system(f'timeout 5 python3 get_numbers.py <_test_input_{ID} >_test_output_{ID}')
+    try:
+        output = numpy.loadtxt(f'_test_output_{ID}', delimiter='\t')
+        assert output.shape == (2,)
+        guess1, guess2 = output
+        assert 0 < guess1 < 100 and 0 < guess2 < 100
+    except:
+        print(f'{round:4d}: FAIL!')
+        return -100
+    result = data[round][0]
+    guesses = numpy.concatenate([[guess1, guess2], data[round][1:]])
+    winner = abs(guesses - result).argmin()
+    score = winner in (0, 1)
+    print(f'{round:4d}: score={score:d}, result={result}, guess1={guess1}, guess2={guess2}, best={guesses[winner]}')
+    return score
+
+
+def main(file, test_times):
+    global ID, TEST_TIMES, data, column_count, row_count
+
+    ID = f'{file.replace("/", "_")}_{test_times}'
+    TEST_TIMES = test_times
+
+    path = f'tests/{file}.txt'
+    if not os.path.exists(path):
+        path = file
+    data = numpy.loadtxt(path, skiprows=1)
+    row_count, column_count = data.shape
+    result = sum(test_round(i) for i in range(row_count))
+    print('result:', result)
+
+
+if __name__ == '__main__':
+    from sys import argv
+    main(argv[1], int(argv[2]))
